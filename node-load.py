ec195021b1c080e2ab6f272fa4b7ff5d6550c39a
Ben Dale <ben.dale@gmail.com>
2015-06-08 20:45:29 +1000

    added sqlite3 db creation and interface insertion

--- node-load.py
+++ node-load.py
@@ -3,6 +3,7 @@
 ## node-load - import device interfaces into sqlite db
 
 import re, sys, getopt
+import sqlite3
 from jnpr.junos import Device
 from jnpr.junos.op.ethport import EthPortTable
 from getpass import getpass
@@ -37,36 +38,56 @@ def main(argv):
 	sys.stdout.write("x - error\n")
 
 	deviceInventory = []
+	dbcon = None
 	
-	# Regex for routable IP addresses (1.0.0.0-223.255.255.255)
-	inetRegex = re.compile("^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-1][0-9]|22[0-3])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$") 
-	hostAddress = sys.argv[1]
+	# Open connection to db
+	try:
+		dbcon = sqlite3.connect('lattice.db')
+    
+		cur = dbcon.cursor()    
+
+		cur.execute('DROP TABLE IF EXISTS PortTable')
+		cur.execute('CREATE TABLE PortTable (PortID INTEGER PRIMARY KEY ASC, PortName TEXT NOT NULL)')
+    
+		# Regex for routable IP addresses (1.0.0.0-223.255.255.255)
+		inetRegex = re.compile("^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-1][0-9]|22[0-3])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$") 
+		hostAddress = sys.argv[1]
+
+		# Open device, retrieve Serial Number and Model
+		if inetRegex.match(str(hostAddress)):
+			sys.stdout.write('.')
+			sys.stdout.flush()
+			if password != '':
+				dev = Device(host=hostAddress.rstrip('\n'),user=username,passwd=password)
+			else:
+				dev = Device(host=hostAddress.rstrip('\n'),user=username)
+			dev.open()	
+			deviceInventory.append({"IP Address":str(hostAddress).rstrip('\n'),"Serial Number":dev.facts['serialnumber'],"Model":dev.facts['model']})
+			print "\nIP Address    ", deviceInventory[0]['IP Address'] 
+			print "Serial Number ", deviceInventory[0]['Serial Number']
+			print "Model         ", deviceInventory[0]['Model']
+			portInventory = EthPortTable(dev)
+			portInventory.get()
+			count = 0
+			# Dump interfaces
+			print "Interfaces:"
+			for port in portInventory:
+				print port.name
+				cur.execute("INSERT INTO PortTable(PortName) VALUES(?)",(port.name,))
+			dbcon.commit()
+			dev.close()
+		else:	
+			sys.stdout.write("x")
+			sys.stdout.flush()
+			deviceInventory.append({"IP Address":str(hostAddress).rstrip('\n'),"Serial Number":"IP Address Error","Model":"N/A"})
+
+	except sqlite3.Error, e:
+		print "Error %s:" % e.args[0]
+		sys.exit(1)	
 	
-	# Open device, retrieve Serial Number and Model
-	if inetRegex.match(str(hostAddress)):
-		sys.stdout.write('.')
-		sys.stdout.flush()
-		if password != '':
-			dev = Device(host=hostAddress.rstrip('\n'),user=username,passwd=password)
-		else:
-			dev = Device(host=hostAddress.rstrip('\n'),user=username)
-		dev.open()	
-		deviceInventory.append({"IP Address":str(hostAddress).rstrip('\n'),"Serial Number":dev.facts['serialnumber'],"Model":dev.facts['model']})
-		print "\nIP Address    ", deviceInventory[0]['IP Address'] 
-		print "Serial Number ", deviceInventory[0]['Serial Number']
-		print "Model         ", deviceInventory[0]['Model']
-		portInventory = EthPortTable(dev)
-		portInventory.get()
-		count = 0
-		# Dump interfaces
-		print "Interfaces:"
-		for port in portInventory:
-			print port.name
-		dev.close()
-	else:	
-		sys.stdout.write("x")
-		sys.stdout.flush()
-		deviceInventory.append({"IP Address":str(hostAddress).rstrip('\n'),"Serial Number":"IP Address Error","Model":"N/A"})
+	finally:
+		if dbcon:
+			dbcon.close()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
