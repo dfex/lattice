255a0653f52daf26cb5c3f3e9433fbff33393917
Ben Dale <ben.dale@gmail.com>
2015-06-12 16:32:41 +1000

    moved db creation to function

--- node-load.py
+++ node-load.py
@@ -9,6 +9,23 @@ from jnpr.junos.op.ethport import EthPortTable
 from getpass import getpass
 from pprint import pprint
 
+
+def populateDB(dbconnection):
+	cur = dbconnection.cursor()    
+
+	cur.execute('DROP TABLE IF EXISTS PortTable')
+	cur.execute('CREATE TABLE CustomerTable (CustomerID INTEGER PRIMARY KEY ASC, CustomerName TEXT, BillingEmailAddress TEXT, CustomerStatus TEXT)')
+	cur.execute('CREATE TABLE UserTable (UserID INTEGER PRIMARY KEY ASC, UserName TEXT, UserPassword TEXT, UserStatus TEXT)')
+	cur.execute('CREATE TABLE LocationTable (LocationID INTEGER PRIMARY KEY ASC, LocationName TEXT NOT NULL, RackID TEXT, RackUnit INTEGER, FacilityName TEXT NOT NULL, FacilityFloor TEXT, FacilityAddress TEXT NOT NULL, FacilityState TEXT NOT NULL, Facility Country TEXT NOT NULL, LocationStatus TEXT NOT NULL)')
+    cur.execute('CREATE TABLE NodeTable (NodeID INTEGER PRIMARY KEY ASC, NodeName TEXT NOT NULL, NodeType TEXT NOT NULL, NodeIPAddress TEXT NOT NULL, FOREIGN KEY(LocationID) REFERENCES LocationTable(LocationID), NodeStatus TEXT NOT NULL)')
+	cur.execute('CREATE TABLE PortTable (PortID INTEGER PRIMARY KEY ASC, PortName TEXT NOT NULL, PortStatus TEXT, PortSpeed INTEGER, FOREIGN KEY(CustomerID REFERENCES CustomerTable(CustomerID), FOREIGN KEY(NodeID) REFERENCES NodeTable(NodeID))')
+    cur.execute('CREATE TABLE ServicesTable (ServiceID INTEGER PRIMARY KEY ASC, ServiceName TEXT NOT NULL, ServiceType TEXT NOT NULL, ServiceRouteTarget TEXT, ServiceRouteDistinguisher TEXT)')
+    cur.execute('CREATE TABLE SubInterfaceTable (SubInterfaceID INTEGER PRIMARY KEY ASC, SubInterfaceUnit INTEGER, SubInterfaceVLANID INTEGER, FOREIGN KEY(ServiceID) REFERENCES ServiceTable(ServiceID), SubInterfaceStatus TEXT NOT NULL, FOREIGN KEY(PortID) REFERENCES ServiceTable(PortID)')
+    cur.execute('CREATE TABLE AuthorisationTable (FOREIGN KEY(CustomerID) REFERENCES CustomerTable(CustomerID), FOREIGN KEY(UserID TEXT) REFERENCES UserTable(Username), AuthorisationRole TEXT, FOREIGN KEY(PortID) REFERENCES PortTable(PortID), FOREIGN KEY(SubInterfaceID) REFERENCES SubInterfaceTable(SubInterfaceID)')
+	cur.execute('CREATE TABLE ServiceMappingTable (FOREIGN KEY(ServiceID) REFERENCES ServiceTable(ServiceID), FOREIGN KEY(SubInterfaceID) REFERENCES SubInterfaceTable(SubInterfaceID))')
+	cur.execute('CREATE TABLE TransactionLog (TransactionID INTEGER PRIMARY KEY ASC, TransactionTimeStamp TEXT, FOREIGN KEY(UserID) REFERENCES UserTable(UserID), IPAddress TEXT, EventType TEXT, FOREIGN KEY(PortID) REFERENCES PortTable(PortID), FOREIGN KEY(SubInterfaceID) REFERENCES SubInterfaceTable(SubInterfaceID), EventDescription TEXT)')
+
+
 ##def openNode():
 	## 
 
@@ -52,11 +69,7 @@ def main(argv):
 	try:
 		dbcon = sqlite3.connect('lattice.db')
     
-		cur = dbcon.cursor()    
-
-		cur.execute('DROP TABLE IF EXISTS PortTable')
-		cur.execute('CREATE TABLE PortTable (PortID INTEGER PRIMARY KEY ASC, PortName TEXT NOT NULL)')
-    
+    	populateDB(dbcon)
 		# Regex for routable IP addresses (1.0.0.0-223.255.255.255)
 		inetRegex = re.compile("^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-1][0-9]|22[0-3])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$") 
 		hostAddress = sys.argv[1]
