7d09eef9a538b69191120437a1ec4df13f81577d
Ben Dale <ben.dale@gmail.com>
2015-06-15 23:29:45 +1000

    refactoring script into functions

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # lattice
-```lattice``` is an experiment in Database-driven network programability.
+```lattice``` is an experimental toy for database-driven network programmability.
 
-In its simplest form, ```lattice``` takes network device(s) and loads operational information (interface names, protocol statuses etc) into a SQL (Lite) database, so that higher layer tools can access and process this data in a meaningful way.
-
-```lattice``` is the foundation layer of a number of projects i've had in the back of my mind for some years now and will hopefully kickstart development of some of them!
+```lattice``` takes network device(s) and loads operational information (interface names, protocol statuses etc) into a SQL (Lite) database, so that higher layer tools can access and process this data in a meaningful way.
--- node-load.py
+++ node-load.py
@@ -11,19 +11,30 @@ from pprint import pprint
 
 
 def populateDB(dbconnection):
-	cur = dbconnection.cursor()    
+	cur = dbconnection.cursor()
+	cur.execute("PRAGMA foreign_keys")
+	cur.execute("DROP TABLE IF EXISTS CustomerTable")
+	cur.execute("DROP TABLE IF EXISTS UserTable")
+	cur.execute("DROP TABLE IF EXISTS LocationTable")
+	cur.execute("DROP TABLE IF EXISTS NodeTable")
+	cur.execute("DROP TABLE IF EXISTS PortTable")
+	cur.execute("DROP TABLE IF EXISTS ServiceTable")
+	cur.execute("DROP TABLE IF EXISTS SubInterfaceTable")
+	cur.execute("DROP TABLE IF EXISTS AuthorisationTable")
+	cur.execute("DROP TABLE IF EXISTS ServiceMappingTable")
+	cur.execute("DROP TABLE IF EXISTS TransactionLog")
+	cur.execute("CREATE TABLE CustomerTable (CustomerID INTEGER PRIMARY KEY ASC, CustomerName TEXT, BillingEmailAddress TEXT, CustomerStatus TEXT)")
+	cur.execute("CREATE TABLE UserTable (UserID INTEGER PRIMARY KEY ASC, UserName TEXT, UserPassword TEXT, UserStatus TEXT)")
+	cur.execute("CREATE TABLE LocationTable (LocationID INTEGER PRIMARY KEY ASC, LocationName TEXT NOT NULL, RackID TEXT, RackUnit INTEGER, FacilityName TEXT NOT NULL, FacilityFloor TEXT, FacilityAddress TEXT NOT NULL, FacilityState TEXT NOT NULL, Facility Country TEXT NOT NULL, LocationStatus TEXT NOT NULL)")
+	cur.execute("CREATE TABLE NodeTable (NodeID INTEGER PRIMARY KEY ASC, NodeName TEXT NOT NULL, NodeType TEXT NOT NULL, NodeIPAddress TEXT NOT NULL, LocationID TEXT, NodeStatus TEXT NOT NULL)")
+	cur.execute("CREATE TABLE PortTable (PortID INTEGER PRIMARY KEY ASC, PortName TEXT NOT NULL, PortStatus TEXT, PortSpeed INTEGER, CustomerID TEXT, NodeID TEXT)")
+	cur.execute("CREATE TABLE ServiceTable (ServiceID INTEGER PRIMARY KEY ASC, ServiceName TEXT NOT NULL, ServiceType TEXT NOT NULL, ServiceRouteTarget TEXT, ServiceRouteDistinguisher TEXT)")
+	cur.execute("CREATE TABLE SubInterfaceTable (SubInterfaceID INTEGER PRIMARY KEY ASC, SubInterfaceUnit INTEGER, SubInterfaceVLANID INTEGER, SubIntServiceID TEXT, SubInterfaceStatus TEXT NOT NULL, PortID TEXT)")
+	cur.execute("CREATE TABLE AuthorisationTable (CustomerID TEXT, UserName TEXT, AuthorisationRole TEXT, PortID TEXT, SubInterfaceID TEXT)")
+	cur.execute("CREATE TABLE ServiceMappingTable (ServiceID TEXT, SubInterfaceID TEXT)")
+	cur.execute("CREATE TABLE TransactionLog (TransactionID INTEGER PRIMARY KEY ASC, TransactionTimeStamp TEXT, UserID TEXT, IPAddress TEXT, EventType TEXT, PortID TEXT, SubInterfaceID TEXT, EventDescription TEXT)")
+	return cur
 
-	cur.execute('DROP TABLE IF EXISTS PortTable')
-	cur.execute('CREATE TABLE CustomerTable (CustomerID INTEGER PRIMARY KEY ASC, CustomerName TEXT, BillingEmailAddress TEXT, CustomerStatus TEXT)')
-	cur.execute('CREATE TABLE UserTable (UserID INTEGER PRIMARY KEY ASC, UserName TEXT, UserPassword TEXT, UserStatus TEXT)')
-	cur.execute('CREATE TABLE LocationTable (LocationID INTEGER PRIMARY KEY ASC, LocationName TEXT NOT NULL, RackID TEXT, RackUnit INTEGER, FacilityName TEXT NOT NULL, FacilityFloor TEXT, FacilityAddress TEXT NOT NULL, FacilityState TEXT NOT NULL, Facility Country TEXT NOT NULL, LocationStatus TEXT NOT NULL)')
-    cur.execute('CREATE TABLE NodeTable (NodeID INTEGER PRIMARY KEY ASC, NodeName TEXT NOT NULL, NodeType TEXT NOT NULL, NodeIPAddress TEXT NOT NULL, FOREIGN KEY(LocationID) REFERENCES LocationTable(LocationID), NodeStatus TEXT NOT NULL)')
-	cur.execute('CREATE TABLE PortTable (PortID INTEGER PRIMARY KEY ASC, PortName TEXT NOT NULL, PortStatus TEXT, PortSpeed INTEGER, FOREIGN KEY(CustomerID REFERENCES CustomerTable(CustomerID), FOREIGN KEY(NodeID) REFERENCES NodeTable(NodeID))')
-    cur.execute('CREATE TABLE ServicesTable (ServiceID INTEGER PRIMARY KEY ASC, ServiceName TEXT NOT NULL, ServiceType TEXT NOT NULL, ServiceRouteTarget TEXT, ServiceRouteDistinguisher TEXT)')
-    cur.execute('CREATE TABLE SubInterfaceTable (SubInterfaceID INTEGER PRIMARY KEY ASC, SubInterfaceUnit INTEGER, SubInterfaceVLANID INTEGER, FOREIGN KEY(ServiceID) REFERENCES ServiceTable(ServiceID), SubInterfaceStatus TEXT NOT NULL, FOREIGN KEY(PortID) REFERENCES ServiceTable(PortID)')
-    cur.execute('CREATE TABLE AuthorisationTable (FOREIGN KEY(CustomerID) REFERENCES CustomerTable(CustomerID), FOREIGN KEY(UserID TEXT) REFERENCES UserTable(Username), AuthorisationRole TEXT, FOREIGN KEY(PortID) REFERENCES PortTable(PortID), FOREIGN KEY(SubInterfaceID) REFERENCES SubInterfaceTable(SubInterfaceID)')
-	cur.execute('CREATE TABLE ServiceMappingTable (FOREIGN KEY(ServiceID) REFERENCES ServiceTable(ServiceID), FOREIGN KEY(SubInterfaceID) REFERENCES SubInterfaceTable(SubInterfaceID))')
-	cur.execute('CREATE TABLE TransactionLog (TransactionID INTEGER PRIMARY KEY ASC, TransactionTimeStamp TEXT, FOREIGN KEY(UserID) REFERENCES UserTable(UserID), IPAddress TEXT, EventType TEXT, FOREIGN KEY(PortID) REFERENCES PortTable(PortID), FOREIGN KEY(SubInterfaceID) REFERENCES SubInterfaceTable(SubInterfaceID), EventDescription TEXT)')
 
 def main(argv):
 	sys.stdout.write("node-load\n\n")
@@ -32,11 +43,6 @@ def main(argv):
 		sys.stdout.write("Usage: node-load <node IP address>\n")
 		sys.exit()
 	
-	username = raw_input('Username: ')
-	password = getpass('Password (leave blank to use SSH Key): ')
-	sys.stdout.write(". - success\n")
-	sys.stdout.write("x - error\n")
-
 	deviceInventory = []
 	dbcon = None
 	
@@ -44,10 +50,15 @@ def main(argv):
 	try:
 		dbcon = sqlite3.connect('lattice.db')
     
-    	populateDB(dbcon)
+		cur = populateDB(dbcon)
+		
 		# Regex for routable IP addresses (1.0.0.0-223.255.255.255)
 		inetRegex = re.compile("^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-1][0-9]|22[0-3])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$") 
 		hostAddress = sys.argv[1]
+		username = raw_input('Username: ')
+		password = getpass('Password (leave blank to use SSH Key): ')
+		sys.stdout.write(". - success\n")
+		sys.stdout.write("x - error\n")
 
 		# Open device, retrieve Serial Number and Model
 		if inetRegex.match(str(hostAddress)):
