663e89016636ec9e5ebaa926ad8cfdf5561bb461
Ben Dale <ben.dale@gmail.com>
2015-06-24 08:12:17 +1000

    Added junos-connect lib for XML RPC

---
+++ junos-connect.py
@@ -0,0 +1,53 @@
+from jnpr.junos import Device
+from os import getenv
+
+
+class Junos(object):
+    """Base class for Junos devices.
+
+    :attr:`hostname`: router hostname
+    :attr:`user`: user for logging into `hostname`
+    :attr:`password`: password for logging into `hostname`
+    :attr:`timeout`: time to wait for a response
+    :attr:`connection`: connection to `hostname`
+    """
+    @property
+    def arp_table(self):
+        """A list of ARP entries.
+
+        :returns: ARP entries
+        :rtype: list
+        """
+        table = []
+        old_table = self.connection.rpc.get_arp_table_information(vpn=self.vpn)
+        for old_entry in old_table:
+            if old_entry.tag != 'arp-table-entry':
+                continue
+            entry = dict(ip_address=old_entry.findtext('ip-address').strip(),
+                         interface=old_entry.findtext('interface-name').strip(),
+                         hostname=self.hostname.strip(),
+                         vpn=self.vpn,
+                         mac_address=old_entry.findtext('mac-address').strip())
+            table.append(entry)
+        return table
+
+    def __init__(self, *args, **kwargs):
+        self.hostname = args[0] if len(args) else kwargs.get('host')
+        self.user = kwargs.get('user', getenv('USER'))
+        self.password = kwargs.get('password')
+        self.timeout = kwargs.get('timeout')
+        self.vpn = kwargs.get('vpn', 'default')
+
+    def _connect(self):
+        """Connect to a device.
+
+        :returns: a connection to a Juniper Networks device.
+        :rtype: ``Device``
+        """
+        if self.password='':
+        	dev = Device(self.hostname, user=self.user)
+        else:
+        	dev = Device(self.hostname, user=self.user, password=self.password)
+        dev.open()
+        dev.timeout = self.timeout
+        return dev
